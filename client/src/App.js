416014512c8d681d74ee7013fddea9ef22dc3895
Omar Bakr <omar@archontech.com>
2022-02-06 03:16:58 +0200

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelListContainer, ChannelContainer, Auth } from './components';
+
+import './App.css';
+import 'stream-chat-react/dist/css/index.css'
+
+const cookies = new Cookies();
+
+const apiKey = 'aqsn2v4n9f6n';
+const authToken = cookies.get('token');
+
+const client = StreamChat.getInstance(apiKey);
+
+if (authToken) {
+  client.connectUser({
+    id: cookies.get('userId'),
+    name: cookies.get('username'),
+    fullName: cookies.get('fullName'),
+    phoneNumber: cookies.get('phoneNumber'),
+    image: cookies.get('avatarURL'),
+    hashedPassword: cookies.get('hashedPassword'),
+  }, authToken);
+}
+
+const App = () => {
+
+  const [createType, setCreateType] = useState('');
+  const [isCreating, setIsCreating] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
+  if (!authToken) return <Auth />
+
+  return (
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          setIsEditing={setIsEditing}
+          setCreateType={setCreateType}
+        />
+        <ChannelContainer
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          setIsEditing={setIsEditing}
+          isEditing={isEditing}
+          createType={createType}
+        />
+      </Chat>
+    </div>
+  );
+}
+
+export default App;
